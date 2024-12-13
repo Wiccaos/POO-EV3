06b629bc20405581efaa4cb52cecfd928e2a92c5
wiccaos <wiccaos.moon@outlook.com>
2024-12-13 04:24:21 -0300

    Agregadas funciones faltantes de la conexión con DB

--- DAL/db_conection.py
+++ DAL/db_conection.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from prettytable import from_db_cursor
 from Auxiliares.Constantes import db_user, db_password, db_host, db_database
 
 # Conexion a DB
@@ -13,7 +14,7 @@ def conexion_db():
     except mysql.connector.Error as err:
         print(f"Error de conexión: {err}")
         return None
-    
+
 # Guarda el usuario en la DB
 def save_user_DB(userId, name, username, email, phone):
     """Guarda al usuario obtenido en jsonplaceholder en la base de datos"""
@@ -36,7 +37,7 @@ def save_post_DB(id_post, post_title, body_post):
         cursor.execute(query, (id_post, post_title, body_post))
         cnx.commit()
         cursor.close()
-    print("\nPost Guardado en la DB correctamente\n")
+        print("\nPost Guardado en la DB correctamente\n")
 
 # Asigna un post a un usuario
 def asign_post():
@@ -56,21 +57,71 @@ def asign_post():
     print("\nEl post se ha asignado correctamente.\n")
 
 # Guarda la Tarea en la DB
-def save_todo():
+def save_todo(id_todo, todo_title, completed):
     """Guarda la Tarea desde Jsonplaceholder en la Base de Datos"""
-    pass
+    cnx = conexion_db()
+    if cnx:
+        cursor = cnx.cursor()
+        query = "INSERT INTO ToDos (id_todo, todo_title, completed) VALUES (%s, %s, %s);"
+        cursor.execute(query, (id_todo, todo_title, completed))
+        cnx.commit()
+        cursor.close()
+        print("\nTarea Guardada en la DB correctamente\n")
 
 # Asigna una Tarea a un usuario
 def asign_todo():
-
+    """Función para asignar una tarea obtenida desde jsonplaceholder en la base de datos"""
     # Obtener datos
-    id_todo = int(input(""))
-    userId = int(input(""))
+    id_todo = int(input("Ingrese el ID de la tarea que desea asignar: "))
+    userId = int(input("Ingrese el ID del usuario a asignar: "))
 
     cnx = conexion_db()
     if cnx:
         cursor = cnx.cursor()
-        query = ""
-        cursor.execute(query, (id_todo, userId))
+        query = "UPDATE ToDos SET User_id_user = %s WHERE id_todo = %s;"
+        cursor.execute(query, (userId, id_todo))
         cnx.commit()
         cursor.close()
+        print("\nLa tarea se ha asignado correctamente.\n")
+
+# Ver Tabla de Usuarios
+def view_user_DB():
+    """Función para ver la tabla de usuarios en la base de datos"""
+    cnx = conexion_db()
+    if cnx:
+        cursor = cnx.cursor()
+        query = "SELECT * FROM User;"
+        cursor.execute(query)
+        try:
+            tabla = from_db_cursor(cursor)
+            print(tabla)
+        except:
+            print("\nNo hay usuarios registrados en la DB\n")
+
+# Ver tabla de Posts
+def view_post_DB():
+    """Función para ver la tabla de posts en la base de datos"""
+    cnx = conexion_db()
+    if cnx:
+        cursor = cnx.cursor()
+        query = "SELECT * FROM Post;"
+        cursor.execute(query)
+        try:
+            tabla = from_db_cursor(cursor)
+            print(tabla)
+        except:
+            print("\nNo hay posts registrados en la DB\n")
+
+# Ver tabla de ToDos
+def view_todo_DB():
+    """Función para ver la tabla de ToDos en la base de datos"""
+    cnx = conexion_db()
+    if cnx:
+        cursor = cnx.cursor()
+        query = "SELECT * FROM ToDos;"
+        cursor.execute(query)
+        try:
+            tabla = from_db_cursor(cursor)
+            print(tabla)
+        except:
+            print("\nNo hay ToDos registrados en la DB\n")
