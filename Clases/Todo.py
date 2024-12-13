11c65faaabde61617174108c5ca41cc1e022eb59
wiccaos <wiccaos.moon@outlook.com>
2024-12-13 03:28:26 -0300

    editada la clase de "todo"

--- Clases/Todo.py
+++ Clases/Todo.py
@@ -1,6 +1,6 @@
 class Todo:
     """Clase de lista de cosas por hacer 'To Do's'"""
-    def __init__(self, userId = 0, Id= 0, title = '', completed = True):
+    def __init__(self, userId = 0, Id= 0, title = '', completed = ''):
         self.userId = userId
         self.Id = Id
         self.title = title
