357b6153a3df84658602a0dc2b6f83ace4c0b7c2
𝑰𝒏̃𝒂𝒌𝒊 𝒁𝒂́𝒓𝒂𝒕𝒆 愛 <zaarate7ignacio@gmail.com>
2024-12-15 11:46:49 -0300

    Integración de SERPER api, y key.

--- Auxiliares/Constantes.py
+++ Auxiliares/Constantes.py
@@ -1,8 +1,14 @@
+# Jsonplaceholder API
 URL_JSONPLACEHOLDER = "https://jsonplaceholder.typicode.com/"
 
 URL_Post = "https://jsonplaceholder.typicode.com/posts"
 URL_Todo = "https://jsonplaceholder.typicode.com/todos"
 URL_Users = "https://jsonplaceholder.typicode.com/users"
+
+# Serper API
+SERPER_API_KEY = '436a2e8ebca22ffebd458b5bc7087ddf1ffb6574'  # Reemplaza con tu clave de API SERPER
+SERPER_URL = "https://google.serper.dev/search"  # URL de la API SERPER
+
 
 # DB
 db_host = "localhost"
